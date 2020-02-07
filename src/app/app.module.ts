6ce695f34c0c299fbbce012547f7ffbf83e04535
VaniaStoicheva <stoychevavania@gmail.com>
2020-02-07 17:20:38 +0200

    School - Angular project with Kinvey

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CoreModule } from './core/core.module';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CauseModule } from './cause/cause.module';
+import { UserModule } from './user/user.module';
+import { CourseModule } from './course/course.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    LoginComponent,
+    RegisterComponent
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    HttpClientModule,
+    AppRoutingModule,
+    CauseModule,
+    UserModule,
+    CourseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
